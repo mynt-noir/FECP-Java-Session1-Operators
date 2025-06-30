5962f5f9c8700c95778a0f5e78c3e549ba49d082
mynt-noir <nuanpatricia.serrano@gcash.com>
2025-06-30 14:42:27 +0800

    feat: add celsius to fahrenheit converter function

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,17 @@
 package org.example;
 
-import java.util.*;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+import java.util.*;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+
+        System.out.printf("Enter temperature in Celsius: ");
+        float temperatureCelsius = scanner.nextFloat();
+        float temperatureFahrenheit = (temperatureCelsius * ((float) 9 /5) + 32);
+        System.out.printf("Temperature in Fahrenheit: %.2f", temperatureFahrenheit);
 
     }
 }
